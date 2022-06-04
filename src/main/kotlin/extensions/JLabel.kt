279c0f8924bf729cbabc1736ad4b648a92ae1d44
Jaemin.Park <once29@naver.com>
2022-06-05 02:41:44 +0900

    Delete extensions

--- src/main/kotlin/extensions/JLabel.kt
+++
@@ -1,9 +0,0 @@
-package extensions
-
-import javax.swing.JLabel
-import javax.swing.SwingConstants
-
-fun JLabel.setAlignmentCenter() {
-    horizontalAlignment = SwingConstants.CENTER
-    verticalAlignment = SwingConstants.CENTER
-}
--- src/main/kotlin/extensions/JTable.kt
+++
@@ -1,7 +0,0 @@
-package extensions
-
-import javax.swing.JTable
-
-fun JTable.selectedFirstColumnOfRaw(): Any {
-    return getValueAt(selectedRow, 0)
-}
