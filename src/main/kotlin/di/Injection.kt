c72b2b5304579524b37dabec7e09ac63e325ce43
Jaemin.Park <once29@naver.com>
2022-06-16 20:23:01 +0900

    Delete MainViewModel

--- src/main/kotlin/di/Injection.kt
+++ src/main/kotlin/di/Injection.kt
@@ -1,7 +1,7 @@
 package di
 
+import db.repository.*
 import ui.MainView
-import ui.MainViewModel
 import ui.View
 
 object Injection {
@@ -9,14 +9,13 @@ object Injection {
     @Volatile
     private var view: View? = null
 
-    @Volatile
-    private var viewModel: MainViewModel? = null
-
     fun provideMainView(): View = view ?: synchronized(this) {
-        view ?: MainView().also { view = it }
-    }
-
-    fun provideViewModel(): MainViewModel = viewModel ?: synchronized(this) {
-        viewModel ?: MainViewModel().also { viewModel = it }
+        view ?: MainView(
+            memberRepository = MemberRepository(),
+            productRepository = ProductRepository(),
+            baristaRepository = BaristaRepository(),
+            orderRepository = OrderRepository(),
+            provideRepository = ProvideRepository()
+        ).also { view = it }
     }
 }
--- src/main/kotlin/ui/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package ui
-
-class MainViewModel {
-
-
-
-}
